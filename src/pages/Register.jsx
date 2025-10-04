3b05c92977660ee4eb0bf230ca30dccc982805ea
Mohammad Reza Mehdizadeh <morez.mehdizadeh1999@gmail.com>
2025-10-04 20:45:55 +0330

    register page completed

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,52 +1,87 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import Input from "../components/ui/Input";
+import Button from "../components/ui/Button";
 
-const Register = () => {
+const Login = () => {
+  const [form, setForm] = useState({ username: "", email: "" , password: "" , confirmPassword: "" });
+
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    console.log(form);
+  };
+
   return (
     <div className="flex w-full max-w-5xl bg-black">
-      <div className="flex-1">
-        <img
-          src="https://picsum.photos/800/600"
-          alt="register"
-          className="rounded-lg"
-        />
-      </div>
-      <div className="w-1/2 p-8 flex flex-col justify-center">
+      <form className="w-2/3 p-8 flex flex-col justify-center">
         <h2 className="text-lg font-bold mb-6">ثبت نام</h2>
 
-        <input
+        <Input
           type="text"
-          placeholder="نام خود را وارد نمایید"
-          className="mb-4 w-full rounded bg-zinc-900 px-3 py-2 text-sm text-white"
-        />
-        <input
-          type="email"
-          placeholder="ایمیل خود را وارد نمایید"
-          className="mb-4 w-full rounded bg-zinc-900 px-3 py-2 text-sm text-white"
-        />
-        <input
-          type="password"
-          placeholder="رمز عبور خود را وارد نمایید"
-          className="mb-4 w-full rounded bg-zinc-900 px-3 py-2 text-sm text-white"
-        />
-        <input
-          type="password"
-          placeholder="رمز عبور خود را دوباره وارد نمایید"
-          className="mb-4 w-full rounded bg-zinc-900 px-3 py-2 text-sm text-white"
+          label="نام و نام خانوادگی"
+          name="username"
+          value={form.username}
+          onChange={handleChange}
+          placeholder="نام و نام خانوادگی"
         />
 
-        <button className="bg-pink-600 hover:bg-pink-700 rounded px-4 py-2">
-          ثبت نام
-        </button>
+        <Input
+          type="text"
+          label="ایمیل"
+          name="email"
+          value={form.email}
+          onChange={handleChange}
+          placeholder="ایمیل"
+        />
+
+        <Input
+          type="password"
+          label="رمز عبور"
+          name="password"
+          value={form.password}
+          onChange={handleChange}
+          placeholder="••••••"
+        />
+
+        <Input
+          type="password"
+          label="تکرار رمز عبور"
+          name="confirmPassword"
+          value={form.confirmPassword}
+          onChange={handleChange}
+          placeholder="••••••"
+        />
+
+        <Button
+          className="mt-4 w-full bg-[var(--color-pink-primary)] card-custom py-2 rounded text-white"
+          type="submit"
+          onClick={handleClick}
+          disabled={false}
+        >
+          ورود
+        </Button>
 
         <p className="mt-4 text-sm">
-          عضو هستید؟{" "}
+          عضو هستید ؟
           <Link to="/login" className="text-pink-500">
-            ورود
+            {`       ورود`}
           </Link>
         </p>
+      </form>
+
+      <div className="">
+        <img
+          src="/public/auth-light.png"
+          alt="login"
+          className="rounded-lg w-full"
+        />
       </div>
     </div>
   );
 };
 
-export default Register;
+export default Login;
