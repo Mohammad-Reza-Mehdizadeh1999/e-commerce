08c1508313c164833d826294369094e1360d685f
Mohammad Reza Mehdizadeh <morez.mehdizadeh1999@gmail.com>
2025-10-13 23:20:23 +0330

    feature: add star rating to product information

--- src/components/SingleProductInformation.jsx
+++ src/components/SingleProductInformation.jsx
@@ -3,8 +3,9 @@ import { FaStar, FaRegHeart } from "react-icons/fa";
 import { BsInfoCircle, BsShopWindow } from "react-icons/bs";
 import { FiShoppingCart } from "react-icons/fi";
 import { CiShoppingBasket } from "react-icons/ci";
+import StarRating from "./ui/StarRating";
 
-const SingleProductInformation = ({product , productCategory}) => {
+const SingleProductInformation = ({ product, productCategory }) => {
   return (
     <div className="flex flex-col md:flex-row justify-between items-center bg-black text-white rounded-2xl p-6 md:p-10 gap-10">
       <div className="w-full md:w-1/2 flex justify-center">
@@ -19,9 +20,7 @@ const SingleProductInformation = ({product , productCategory}) => {
 
       <div className="flex flex-col justify-center w-full md:w-1/2 space-y-5">
         <div className="flex items-center justify-between">
-          <h2 className="text-2xl md:text-3xl font-semibold">
-            {product.name}
-          </h2>
+          <h2 className="text-2xl md:text-3xl font-semibold">{product.name}</h2>
           <button
             className="pl-5 text-gray-400 hover:text-pink-500 transition cursor-pointer"
             title="Add to favorites"
@@ -30,35 +29,40 @@ const SingleProductInformation = ({product , productCategory}) => {
           </button>
         </div>
 
-        <p className="text-gray-400 leading-relaxed">
-          {product.description}
-        </p>
+        <p className="text-gray-400 leading-relaxed">{product.description}</p>
 
-        <p className="text-4xl font-bold mt-4">{product.price.toLocaleString()} تومان</p>
+        <p className="text-4xl font-bold mt-4">
+          {product.price.toLocaleString()} تومان
+        </p>
 
         <div className="flex items-center justify-between w-1/2 text-gray-300 mt-3 text-sm">
           <div className="space-y-2">
             <p className="flex items-center gap-2">
               <FiShoppingCart />
-              <span className="font-semibold">تعداد :</span> {product.quantity} عدد
+              <span className="font-semibold">تعداد :</span> {product.quantity}{" "}
+              عدد
             </p>
             <p className="flex items-center gap-2">
               <CiShoppingBasket />
-              <span className="font-semibold">موجودی :</span> {product.countInStock}
+              <span className="font-semibold">موجودی :</span>{" "}
+              {product.countInStock}
             </p>
             <p className="flex items-center gap-2">
               <FaStar />
-              <span className="font-semibold">امتیاز :</span> {Math.round(product.rating * 100) / 100}
+              <span className="font-semibold">امتیاز :</span>{" "}
+              {Math.round(product.rating * 100) / 100}
             </p>
           </div>
 
           <div className="space-y-2">
             <p className="flex items-center gap-2">
               <BsShopWindow />
-              <span className="font-semibold">برند :</span> {productCategory?.name}
+              <span className="font-semibold">برند :</span>{" "}
+              {productCategory?.name}
             </p>
             <p className="flex items-center gap-2">
-              <BsInfoCircle /> زمان بروز رسانی :   {new Date(product.updatedAt).toLocaleDateString("en-US")}
+              <BsInfoCircle /> زمان بروز رسانی :{" "}
+              {new Date(product.updatedAt).toLocaleDateString("en-US")}
             </p>
             <p className="flex items-center gap-2">
               <FaStar /> نظرات : {product.numReviews}
@@ -67,10 +71,8 @@ const SingleProductInformation = ({product , productCategory}) => {
         </div>
 
         <div className="flex items-center gap-2 mt-4">
-          {[...Array(5)].map((_, i) => (
-            <FaStar key={i} className="text-yellow-400" />
-          ))}
-          <span className="text-gray-400 text-sm">۵۰۰۰ نظر</span>
+          <StarRating rating={product.rating || 0} />
+          <span className="text-gray-400 text-sm">{product.numReviews} نظر </span>
         </div>
 
         <div className="flex items-center gap-4 mt-6">
